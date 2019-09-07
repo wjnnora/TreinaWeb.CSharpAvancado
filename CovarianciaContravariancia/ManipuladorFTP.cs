f545df0dc338fdc139094999990fbf9739c15e15
wjnnora <wellington_nora94@hotmail.com>
2019-09-07 18:41:19 -0300

    segundo commit

---
+++ CovarianciaContravariancia/ManipuladorFTP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovarianciaContravariancia
+{
+    public class ManipuladorFTP<T> : IArmazenador<T>, IRecuperador<T>
+    {
+        private List<T> _lista = new List<T>();
+        public void Armazenador(T item)
+        {
+            this._lista.Add(item);
+        }
+
+        public T Recuperador(int id)
+        {
+            return _lista[id];
+        }
+    }
+}
--- CovarianciaContravariancia/Program.cs
+++ CovarianciaContravariancia/Program.cs
@@ -10,7 +10,25 @@ namespace CovarianciaContravariancia
     {
         static void Main(string []args)
         {
+            ManipuladorFTP<string> manipuladorFTP = new ManipuladorFTP<string>();
+            manipuladorFTP.Armazenador("wellington_nora94@hotmail.com");
+            manipuladorFTP.Armazenador("welintonnora@gmail.com");
 
+            Console.WriteLine(manipuladorFTP.Recuperador(1));
+
+            Console.ReadKey();
+
+            Object pessoa = new 
+            {
+                Nome = "Wellington Junior Nora",
+                Idade = 24
+            };
+
+            var nome = pessoa.GetType().GetProperty("Nome") != null ? pessoa.GetType().GetProperty("Nome").GetValue(pessoa, null) : "";
+            var idade = pessoa.GetType().GetProperty("Idade") != null ? pessoa.GetType().GetProperty("Idade").GetValue(pessoa, null) : "";
+
+            Console.WriteLine($"O Sr. {nome} tem {idade} de idade");
+            Console.ReadKey();
         }
     }
 }
